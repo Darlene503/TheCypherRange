35ab87744a46657ea5d6fb5dc53b357e067260c0
Darlene503 <duyn503@gmail.com>
2023-09-06 18:24:31 -0700

    trying to get database to connect

--- TheCypherRange/Models/Albums.cs
+++ TheCypherRange/Models/Albums.cs
@@ -5,13 +5,9 @@
         public int AlbumID { get; set; }
         public required string AlbumTitle { get; set; }
         public int ReleaseDate { get; set; }    
+        public int ArtistName { get; set;}   
 
-        public int  ArtistID { get; set; }
-        public int ArtistName { get; set;}
-
-        public int SongID { get; set;}
-
-        public required string SongName { get; set;}
+        
 
 
 
